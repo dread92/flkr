1251197a162300afdb13826b89c7caafd193142d
dread92 <dread+github@dread-space.com>
2018-07-14 11:13:39 -0400

    spring boot project
    A spring boot project that will try to replicate flikr

---
+++ src/main/java/io/reade/flkr/FlkrApplication.java
@@ -0,0 +1,12 @@
+package io.reade.flkr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlkrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlkrApplication.class, args);
+	}
+}
