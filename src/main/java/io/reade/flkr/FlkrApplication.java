16391aa1c4ac140c8bf50e5f4db3d8bfa1dec1b7
David <dread+github@dread-space.com>
2018-07-20 19:26:45 +0000

    New things for spring-boot learning

--- src/main/java/io/reade/flkr/FlkrApplication.java
+++ src/main/java/io/reade/flkr/FlkrApplication.java
@@ -1,12 +1,66 @@
 package io.reade.flkr;
 
+import io.reade.flkr.storage.StorageService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+
+import io.reade.flkr.storage.StorageProperties;
+import io.reade.flkr.storage.StorageService;
 
 @SpringBootApplication
+@EnableConfigurationProperties(StorageProperties.class)
 public class FlkrApplication {
+
+    public static final Logger log = LoggerFactory.getLogger(FlkrApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(FlkrApplication.class, args);
 	}
+
+	@Bean
+	public CommandLineRunner demo(UserRepository repository) {
+		return args -> {
+			repository.save(new User("Keturah", "Bauer"));
+			repository.save(new User("Bob", "Bauer"));
+			repository.save(new User("April", "O'Brian"));
+			repository.save(new User("Rachel", "Palmer"));
+			repository.save(new User("Shiloh", "Dessler"));
+
+			log.info("Customers found with findAll():");
+			log.info("-------------------------------");
+			for (User user : repository.findAll()) {
+			    log.info(user.toString());
+            }
+            log.info("");
+
+			repository.findById(1L)
+                    .ifPresent(user -> {
+                        log.info("User found with findById(1L):");
+                        log.info("-----------------------------");
+                        log.info(user.toString());
+                        log.info("");
+
+                    });
+			log.info("Users found with findByLastName('Bauer'):");
+            log.info("-----------------------------------------");
+            repository.findByLastName("Bauer").forEach(bauer -> {
+                log.info(bauer.toString());
+            });
+			log.info("");
+		};
+	}
+
+	@Bean
+    CommandLineRunner init(StorageService storageService) {
+	    return (args) -> {
+	        storageService.deleteAll();
+	        storageService.init();
+        };
+    }
 }
---
+++ src/main/java/io/reade/flkr/storage/StorageFileNotFoundException.java
@@ -0,0 +1,12 @@
+package io.reade.flkr.storage;
+
+public class StorageFileNotFoundException extends StorageException {
+
+    public StorageFileNotFoundException(String message) {
+        super(message);
+    }
+
+    public StorageFileNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
